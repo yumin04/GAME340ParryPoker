64d7156e25b8eef8146b40f5d528eb8f708006a0
yumin04 <yumin@chapman.edu>
2025-11-10 22:37:14 -0800

    Adding Attack Functionality

--- Assets/Scripts/Attack/Attack.cs
+++ Assets/Scripts/Attack/Attack.cs
@@ -1,16 +1,103 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    [SerializeField] private GameObject Background;
-    [SerializeField] private GameObject HittingSpace;
-    [SerializeField] private GameObject Indicator;
+    private static Attack instance;
+    public static Attack GetInstance() => instance;
     
+    [SerializeField] private GameObject background;     // 초록
+    [SerializeField] private GameObject hittingSpace;   // 빨강
+    [SerializeField] private GameObject indicator;      // 파랑 (내부에서 움직이는 선)
     
-    // Initialize all 3 game object
-    // Background initialized below this object,
-    // hitting space and indicator will be below background
+    [SerializeField] private float indicatorSpeed = 3f;
+    [SerializeField] private float comboResetTime = 1f;
+
+    private bool isMovingRight = true;
+    private int combo = 0;
     
-    // OnPlayerMainAction
-    // int combo
+    private float currentRandomRatio;
+    private float bgHeight;
+
+    private GameObject currentIndicatorObject;
+    private GameObject currentBackgroundObject;
+    private int attackRoundRemaining;
+
+    public void Awake()
+    {
+        InstantiateOneAttackObject();
+    }
+
+    public void OnEnable()
+    {
+        //GameEvents.OnAttackRoundEnd += CheckAttackRoundRemaining;
+    }
+    public void OnDisable()
+    {
+        //GameEvents.OnAttackRoundEnd -= CheckAttackRoundRemaining;
+    }
+    public void StartAttackLoop()
+    {
+        attackRoundRemaining = 5;
+        Destroy(currentBackgroundObject);
+        InstantiateOneAttackObject();
+    }
+
+    private void CheckAttackRoundRemaining()
+    {
+        if(attackRoundRemaining == 0)
+        {
+            //GameEvents.OnAttackPhaseEnd.Invoke(combo);
+        }
+        attackRoundRemaining--;
+        CheckForCombo();
+        InstantiateOneAttackObject();
+    }
+
+
+
+    private void InstantiateOneAttackObject()
+    {
+        // 1️⃣ Background 생성
+        currentBackgroundObject = Instantiate(background, transform);
+    
+        // 2️⃣ Hitting Space 생성 (랜덤 위치)
+        // Vector3 hitPos = GetRandomHittingSpacePosition();
+        GameObject hit = Instantiate(hittingSpace, currentBackgroundObject.transform);
+        hit.transform.localPosition = GetRandomHittingSpacePosition();
+
+        // 3️⃣ Indicator 생성
+        currentIndicatorObject = Instantiate(indicator, currentBackgroundObject.transform);
+        currentIndicatorObject.transform.localPosition = GetIndicatorPosition();  
+    }
+
+    private Vector3 GetIndicatorPosition()
+    {
+        return new Vector3(0,0,0);
+    }
+
+
+    private Vector3 GetRandomHittingSpacePosition()
+    {
+        // within 0.1 top and bottom, of background
+        currentRandomRatio = Random.Range(-0.4f, 0.4f);
+        Vector3 pos = new Vector3(0,0,0);
+        bgHeight = background.GetComponent<Renderer>().bounds.size.y / 2f;
+        pos.y = bgHeight * currentRandomRatio;
+        return pos;
+    }
+    private void CheckForCombo()
+    {
+        if (CalculateIndicatorOnHittingSpace())
+        {
+            
+        }
+    }
+    private bool CalculateIndicatorOnHittingSpace()
+    {
+        float ratio = currentIndicatorObject.transform.localPosition.y / bgHeight;
+        bool ans = ratio < currentRandomRatio + 0.1f && ratio > currentRandomRatio - 0.1f;
+        Debug.Log("[DEBUG] Calculating for combo: " + ans.ToString());
+        return ans;
+    }
 }
--- Assets/Scripts/Attack/Indicator.cs
+++ Assets/Scripts/Attack/Indicator.cs
@@ -3,9 +3,35 @@
 public class Indicator : MonoBehaviour
 {
     private int movementDirection;
-    
-    public void Update()
+    private int movementSpeed;
+    private int speedIncrement;
+
+    public void Awake()
     {
-        
+        movementDirection = 1;
+        movementSpeed = 1;
+        speedIncrement = 1;
+    }
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.y += movementSpeed * movementDirection * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    private void ChangeDirection()
+    {
+        movementDirection *= -1;
+    }
+
+    public void OnCollisionEnter2D()
+    {
+        ChangeDirection();
+    }
+
+    public void IncrementMovementSpeed()
+    {
+
+        movementSpeed += speedIncrement;
     }
 }
